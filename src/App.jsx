edbc05c98bf46511689576653c5e7f050a8962d9
Marcos Vinicius <166649039+viniciusx0800@users.noreply.github.com>
2025-12-05 16:28:01 +0000

    refatorar: atualizar estilos e implementar funcionalidade de busca no componente App

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,80 @@
-import './App.css';
+// import './App.css';
+
+// function MyButton() {
+//   return (
+//     <button className='button-en'>BUSCAR</button>
+//   );
+// }
+
+// function App() {
+//   return (
+//     <div className="App">
+//      <h1 className='vaso-h1'>fala vaso</h1>
+//      <input type="text" placeholder="pesquisar"/>
+//      <MyButton />
+     
+//     </div>
+
+//   );
+// }
+
+
+
+// export default App;
+
+import { useState } from "react";
+import "./App.css";
+
+function MyButton({ onClick }) {
+  return (
+    <button className="button-en" onClick={onClick}>
+      BUSCAR
+    </button>
+  );
+}
 
 function App() {
+  const [query, setQuery] = useState("");
+  const [resultados, setResultados] = useState([]);
+
+  async function buscarMusicas() {
+    const resposta = await fetch(
+      `https://itunes.apple.com/search?term=${query}&media=music`
+    );
+    const dados = await resposta.json();
+    setResultados(dados.results);
+  }
+
   return (
     <div className="App">
-     <h1 className='vaso-h1'>fala vaso</h1>
-     <input type="text" />
-     <button>
-      link
-     </button>
-    </div>
+      <h1 className="vaso-h1">Palco Mp3</h1>
 
+      <input
+        type="text"
+        placeholder="pesquisar"
+        value={query}
+        onChange={e => setQuery(e.target.value)}
+      />
+
+      <MyButton onClick={buscarMusicas} />
+      <section>
+      <ul>
+        {resultados.map((musica) => (
+          <li key={musica.trackId} style={{marginBottom:"20px"}}>
+            <strong>{musica.trackName}</strong> - {musica.artistName}
+            <br />
+             {/* Player de áudio */}
+            <audio controls src={musica.previewUrl}>
+              Seu navegador não suporta áudio.
+            </audio>
+
+          </li>
+        ))}
+      </ul>
+      </section>
+    </div>
   );
 }
 
 export default App;
+
